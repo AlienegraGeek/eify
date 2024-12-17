10a3638a45f3ba533c6e68e9da54aa5ff18ce791
AlienegraGeek <969024145@qq.com>
2024-12-17 20:15:54 +0800

    refactor(core): bump reactflow(v11) to xyflow(v12)

--- src/App.tsx
+++ src/App.tsx
@@ -5,36 +5,35 @@ import './App.css'
 import FlowExample from "./FlowExample.tsx";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-  return (
-      <>
-          <div>
-              <a href="https://vite.dev" target="_blank">
-                  <img src={viteLogo} className="logo" alt="Vite logo" />
-              </a>
-              <a href="https://react.dev" target="_blank">
-                  <img src={reactLogo} className="logo react" alt="React logo" />
-              </a>
-          </div>
-          <h1>Vite + React</h1>
-          <div>
-              <h1 style={{textAlign: 'center'}}>React Flow Example</h1>
-              <FlowExample />
-          </div>
-          {/*<div className="card">*/}
-          {/*    <button onClick={() => setCount((count) => count + 1)}>*/}
-          {/*        count is {count}*/}
-          {/*    </button>*/}
-          {/*    <p>*/}
-          {/*        Edit <code>src/App.tsx</code> and save to test HMR*/}
-          {/*    </p>*/}
-          {/*</div>*/}
-          <p className="read-the-docs">
-              Click on the Vite and React logos to learn more
-          </p>
-      </>
-  )
+    // const [count, setCount] = useState(0)
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div style={{fontFamily: 'Arial, sans-serif', padding: '20px'}}>
+                <h1 style={{textAlign: 'center'}}>XYFlow 示例</h1>
+            </div>
+            <FlowExample />
+            {/*<div className="card">*/}
+            {/*    <button onClick={() => setCount((count) => count + 1)}>*/}
+            {/*        count is {count}*/}
+            {/*    </button>*/}
+            {/*    <p>*/}
+            {/*        Edit <code>src/App.tsx</code> and save to test HMR*/}
+            {/*    </p>*/}
+            {/*</div>*/}
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    )
 }
 
 export default App
--- src/FlowExample.tsx
+++ src/FlowExample.tsx
@@ -1,43 +1,42 @@
-import React from 'react';
-import ReactFlow, {Background, BackgroundVariant, Controls, Edge, Node} from 'reactflow';
-import 'reactflow/dist/style.css'; // 引入 React Flow 的样式
+import React, {useCallback} from 'react';
+import {
+    ReactFlow,
+    Background,
+    Controls,
+    MiniMap,
+    addEdge,
+    useNodesState,
+    useEdgesState,
+    type OnConnect,
+} from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+import {initialNodes, nodeTypes} from './nodes';
+import {initialEdges, edgeTypes} from './edges';
 
-const initialNodes: Node[] = [
-    {
-        id: '1',
-        position: {x: 100, y: 100},
-        data: {label: 'Node 1'},
-        type: 'default',
-    },
-    {
-        id: '2',
-        position: {x: 300, y: 100},
-        data: {label: 'Node 2'},
-        type: 'default',
-    },
-];
-
-const initialEdges: Edge[] = [
-    {
-        id: 'e1-2',
-        source: '1',
-        target: '2',
-        animated: true,
-    },
-];
 
 const FlowExample: React.FC = () => {
+    const [nodes, , onNodesChange] = useNodesState(initialNodes); // 节点状态
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges); // 连线状态
+    const onConnect: OnConnect = useCallback(
+        (connection) => setEdges((edges) => addEdge(connection, edges)),
+        [setEdges]
+    );
+
     return (
-        <div style={{height: '500px', width: '100%'}}>
-            <ReactFlow
-                nodes={initialNodes}
-                edges={initialEdges}
-                fitView
-            >
-                <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
-                <Controls />
-            </ReactFlow>
-        </div>
+        <ReactFlow
+            nodes={nodes}
+            nodeTypes={nodeTypes}
+            onNodesChange={onNodesChange}
+            edges={edges}
+            edgeTypes={edgeTypes}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            fitView
+        >
+            <Background />
+            <MiniMap />
+            <Controls />
+        </ReactFlow>
     );
 };
 
